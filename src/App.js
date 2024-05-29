58b61352423c5b6d61cb5de3bc72883244f61b22
Om Mishra <ommishra162005@gmail.com>
2024-05-29 13:38:24 +0530

    effects class commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Products from './Products';
+import Navbar from './Navbar';
+import TypeWrite from './TypeWrite';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
       </header>
+      <TypeWrite text={[
+            "Hello, I am a student!",
+            "I am an engineer!",
+            "Welcome to my Portfolio!",
+          ]}
+          typingSpeed={100}
+        deletingSpeed={50}
+        duration={1000}
+        
+        />
     </div>
   );
 }
+
+
 
 export default App;
